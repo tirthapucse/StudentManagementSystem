e3e0af95c112a986bba9dd59cb399c2b53c7763a
tirthapucse <tirtha.pucse2018@gmail.com>
2023-08-19 02:21:44 +0600

    html

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,41 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Employee</title>
+    <title>Admin Registration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@l.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
+
 <body>
-        <div class="container">
-            <form action="{{ '/store' }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
-                </div>
+    <nav class="navbar navbar-expand-sm bg-dark">
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                </div>
+        <!-- Links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-light" href="/">Admin Register Here</a>
+            </li>
+        </ul>
 
-                <div class="form-group">
-                    <label for="company_name">Company Name</label>
-                    <input type="text" class="form-control" id="company_name" name="company_name" required>
-                </div>
+    </nav>
+    <div class="container">
+        <form action="{{ '/store' }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group pt-4">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
 
-                <div class="form-group">
-                    <label for="designation">Designation</label>
-                    <input type="text" class="form-control" id="designation" name="designation" required>
-                </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
 
-                <div class="form-group">
-                    <label for="image">Image</label>
-                    <input type="file" class="form-control-file" id="image" name="image" accept="image/*" required>
-                </div>
+            <div class="form-group">
+                <label for="company_name">Company Name</label>
+                <input type="text" class="form-control" id="company_name" name="company_name" required>
+            </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>  
+            <div class="form-group">
+                <label for="designation">Designation</label>
+                <input type="text" class="form-control" id="designation" name="designation" required>
+            </div>
+
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" class="form-control-file" id="image" name="image" accept="image/*" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
 </body>
+
 </html>
